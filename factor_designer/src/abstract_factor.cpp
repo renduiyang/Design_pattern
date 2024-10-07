693a0cab5bee1024a691da06c28e682b573955eb
renduiyang <3072085859@qq.com>
2024-10-07 11:39:27 +0800

    重构饮料工厂模式并添加静态方法
    - 将 hotDrinkFactor 类重命名为 hot_ProductionLine，以更准确地反映其作用
    - 为 drinkFactor 类添加静态方法 make_drink_s，提供另一种创建饮料的方式
    - 优化了代码注释，提高了代码的可读性和维护性

--- factor_designer/src/abstract_factor.cpp
+++ factor_designer/src/abstract_factor.cpp
@@ -41,28 +41,34 @@ std::unique_ptr<hotDrink> make_drink(const std::string &type) {
 
 // ---------使用抽象工厂来实现
 // 此处hotdrinkFactor可以抽象出所有饮品的工厂 是一个虚基类
-class hotDrinkFactor {
+class hot_ProductionLine {
 public:
     virtual std::unique_ptr<hotDrink> make() const = 0;
-    virtual ~hotDrinkFactor() = default;
+
+    virtual ~hot_ProductionLine() = default;
 };
 
-class tea_ProductionLine : public hotDrinkFactor {
+class tea_ProductionLine : public hot_ProductionLine {
 public:
     std::unique_ptr<hotDrink> make() const override {
         return std::make_unique<tea>();
     }
 };
 
-class coffee_ProductionLine : public hotDrinkFactor {
+class coffee_ProductionLine : public hot_ProductionLine {
 public:
     std::unique_ptr<hotDrink> make() const override {
         return std::make_unique<coffee>();
     }
 };
 
+/**
+ * @brief 实例化 工厂类  这个类不需要继承任何东西就是一个单独类
+ * @note 可以理解为这个就是一个工厂里面有两条生产线 一条生产线生成茶 一条生产线生成咖啡
+ * @note 如果需要增加新的饮品  只需要增加一条生产线  也就是新增一个继承于hot_ProductionLine 的一个类
+ */
 class drinkFactor {
-    std::map<std::string, std::unique_ptr<hotDrinkFactor> > hot_factories;
+    std::map<std::string, std::unique_ptr<hot_ProductionLine> > hot_factories;
 
 public:
     drinkFactor() {
@@ -75,11 +81,23 @@ public:
         drink->prepare(200);
         return drink;
     }
+
+    static std::unique_ptr<hotDrink> make_drink_s(const std::string &name) {
+        std::unique_ptr<hotDrink> drink;
+        if (name == "tea") {
+            drink = std::make_unique<tea_ProductionLine>()->make();
+        } else {
+            drink = std::make_unique<coffee_ProductionLine>()->make();
+        }
+        drink->prepare(200);
+        return drink;
+    }
 };
 
 int main() {
     auto tea_drink = make_drink("tea");
     drinkFactor df;
     auto coffee_drink = df.make_drink("coffee");
+    // auto drink_s = drinkFactor::make_drink_s("tea");
     return 0;
 }
